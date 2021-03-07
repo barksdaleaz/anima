620777a82c72c2e9f17649ad73aa05fc0021e00a
am-key <78172503+am-key@users.noreply.github.com>
2021-03-07 13:51:40 -0500

    reflection story update

--- scripts/reflectionscript.js
+++ scripts/reflectionscript.js
@@ -1,11 +1,20 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
-  "Once the water flowed into the fountain, " +
-    "you looked into the fountain and saw your reflection. Now you understand."
+  "Once the water flowed into the fountain, you could see your reflection clearly. Your name was Magnolia. \n" +
+    "I’m Magnolia. \n"
 );
-// var aText = new Array(
-// "Short test",
-// );
+var bText = new Array(
+"Memories about yourself flooded to your mind suddenly. " +
+    "Magnolia is you, and YOU are Magnolia; June 19th is your birthday, " +
+    "your favorite Disney character is Mulan... All the facts that you tried " +
+    "to deny earlier because of low-self-esteem has come back… You are soon relieved. " +
+    "This is me, a gemini who is named after a flower, Magnolia. " +
+    "Magnolia symbolizes dignity and a noble soul. " +
+    "That’s why you like Mulan --- not just because of your similar names. " +
+    "Mulan is a character with self-determination and a strong-mind --- " +
+    "you always admire her femininity. " +
+    "Became proud of yourself as a female and of who you are."
+);
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
@@ -37,5 +46,28 @@ function typewriter()
  }
 }
 
+function typewriter2()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext2");
+
+ while ( iRow < iIndex ) {
+  sContents += bText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + bText[iIndex].substring(0, iTextPos) + "_";
+ iArrLength = bText[iIndex].length;
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != bText.length ) {
+   iArrLength = bText[iIndex].length;
+   setTimeout("typewriter2()", 1000);
+  }
+ } else {
+  setTimeout("typewriter2()", iSpeed);
+ }
+}
 
 typewriter();
+typewriter2();
