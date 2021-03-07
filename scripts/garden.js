4192e438f91d862be728545870176295ab5c9ad3
Yixiao Li <likather@usc.edu>
2021-03-08 00:11:51 +0800

    Update garden.js

--- scripts/garden.js
+++ scripts/garden.js
@@ -1,11 +1,11 @@
 // set up text to print, each item in array is new line
 var aText = new Array("Suddenly you feel a strange sense of worry. The experiment! ",
                       "Something in your head shouts. You rush past the doors and past the hallways leading to the garden. ",
-                      "But how did you know it was the garden?")
+                      "But how did you know it was the garden?");
 var bText = new Array("As you observe your new surroundings, you wonder if it could even be considered a garden. ",
                       "Dark vines coiled around cold, stone columns, and ancient trees leaned over as if threatening to smother you. ",
                       "In the center of the garden was a piece of parchment, with a strange symbol on it.",
-                      "And on each column in the garden, there were letters. What could this mean? ")
+                      "And on each column in the garden, there were letters. What could this mean? ");
 
 var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
