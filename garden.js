606a707e7a14bf2edb1a20811c478390d7a34a57
Yixiao Li <likather@usc.edu>
2021-03-07 09:48:33 +0800

    Create garden.js

---
+++ garden.js
@@ -0,0 +1,9 @@
+// set up text to print, each item in array is new line
+var aText = new Array("Suddenly you feel a strange sense of worry. The experiment! ",
+                      "Something in your head shouts. You rush past the doors and past the hallways leading to the garden. ",
+                      "But how did you know it was the garden?",
+                      "As you observe your new surroundings, you wonder if it could even be considered a garden. ",
+                      "Dark vines coiled around cold, stone columns, and ancient trees leaned over as if threatening to smother you. ",
+                      "In the center of the garden was a piece of parchment, with a strange symbol on it.",
+                      "And on each column in the garden, there were letters. What could this mean? ")
+
