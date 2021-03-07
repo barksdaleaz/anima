3538e5666029b28b1e526cb7f786c14ffc7d8c8e
Amber Barksdale <barksdaleaz@gmail.com>
2021-03-06 19:35:53 -0600

    updated kitchen

--- kitchenscript.js
+++ kitchenscript.js
@@ -2,14 +2,16 @@
 var aText = new Array(
 "You wake up confused on the cold, hard tiles. Your head hurts and your body feels weak.",
 "You try to remember something, anything. But everything comes up blank. ",
-"Your name, what you were doing here, where you were... You couldn't remember anything",
+"Your name, what you were doing here, where you were... You can\'t remember anything.",
 "You stumble and look around to see the cabinets of a fancy kitchen.",
-"On the calendar hanging on the wall, you see a big red circle around June 19, with the words “23rd Birthday!” next to it.",
-"Is that even today’s date? Whose birthday was it?",
-"Next to your body, you see a half-spilled bottle of wine. You lift it up and smell the contents, grimacing at the stench.",
-"Certainly doesn’t smell like wine...You read the label...MOSCATO...."
 );
-var iSpeed = 50; // time delay of print out
+var bText = new Array(
+"On the calendar hanging on the wall, you see a big red circle around June 19, with the words \"23rd Birthday!\" next to it.",
+"Is that even today\'s date? Whose birthday was it?",
+"Next to your body, you see a half-spilled bottle of wine. You lift it up and smell the contents, grimacing at the stench.",
+"Certainly doesn\'t smell like wine...You read the label...MOSCATO...."
+);
+var iSpeed = 100; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
@@ -40,5 +42,28 @@ function typewriter()
  }
 }
 
+function typewriter2()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext2");
+
+ while ( iRow < iIndex ) {
+  sContents += bText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + bText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != bText.length ) {
+   iArrLength = bText[iIndex].length;
+   setTimeout("typewriter2()", 500);
+  }
+ } else {
+  setTimeout("typewriter2()", iSpeed);
+ }
+}
+
 
 typewriter();
+typewriter2();
